19843d603195c2af02841fe60380749c163fd055
Mubashra <mubashramajid725@gmail.com>
2026-08-10 13:25:47 +0500

    conductor-sync-2026-08-10--08-25-47

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router";
 import Navbar from "./Navbar";
-import Footer from "./Footer";
 
 export default function Layout() {
   return (
@@ -9,7 +8,6 @@ export default function Layout() {
       <main className="flex-1 pt-16">
         <Outlet />
       </main>
-      <Footer />
     </div>
   );
 }
